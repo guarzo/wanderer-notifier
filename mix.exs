23bcc19b849df01076ca88548e73bd3e8610fe5a
guarzo <guarzo.eve@gmail.com>
2025-07-14 17:27:20 +0000

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WandererNotifier.MixProject do
   def project do
     [
       app: :wanderer_notifier,
-      version: "4.0.2",
+      version: "4.0.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
