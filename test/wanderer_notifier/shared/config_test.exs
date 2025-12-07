f35f85021cdde85283c41f676c0b4549a4d4e78c
Guarzo <guarzo.eve@gmail.com>
2025-12-07 05:13:38 +0000

    pr feedback

--- test/wanderer_notifier/shared/config_test.exs
+++ test/wanderer_notifier/shared/config_test.exs
@@ -3,106 +3,67 @@ defmodule WandererNotifier.Shared.ConfigTest do
 
   alias WandererNotifier.Shared.Config
 
+  # Sets up corporation_exclude_list config for test and ensures automatic restoration.
+  # Pass :delete to remove the config, or a value to set it.
+  defp with_corporation_exclude_list(value) do
+    original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
+
+    case value do
+      :delete -> Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
+      _ -> Application.put_env(:wanderer_notifier, :corporation_exclude_list, value)
+    end
+
+    on_exit(fn ->
+      case original do
+        nil -> Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
+        _ -> Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
+      end
+    end)
+  end
+
   describe "corporation_exclude_list/0" do
     test "returns empty list when not configured" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
+      with_corporation_exclude_list(:delete)
 
       assert Config.corporation_exclude_list() == []
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      end
     end
 
     test "returns configured list of integers" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.put_env(:wanderer_notifier, :corporation_exclude_list, [98_000_001, 98_000_002])
+      with_corporation_exclude_list([98_000_001, 98_000_002])
 
       assert Config.corporation_exclude_list() == [98_000_001, 98_000_002]
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      else
-        Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
-      end
     end
 
     test "returns single item list when one ID configured" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.put_env(:wanderer_notifier, :corporation_exclude_list, [98_000_001])
+      with_corporation_exclude_list([98_000_001])
 
       assert Config.corporation_exclude_list() == [98_000_001]
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      else
-        Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
-      end
     end
   end
 
   describe "corporation_exclusion_enabled?/0" do
     test "returns false when list is empty" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.put_env(:wanderer_notifier, :corporation_exclude_list, [])
+      with_corporation_exclude_list([])
 
       refute Config.corporation_exclusion_enabled?()
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      else
-        Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
-      end
     end
 
     test "returns false when not configured" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
+      with_corporation_exclude_list(:delete)
 
       refute Config.corporation_exclusion_enabled?()
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      end
     end
 
     test "returns true when list has one entry" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-      Application.put_env(:wanderer_notifier, :corporation_exclude_list, [98_000_001])
+      with_corporation_exclude_list([98_000_001])
 
       assert Config.corporation_exclusion_enabled?()
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      else
-        Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
-      end
     end
 
     test "returns true when list has multiple entries" do
-      original = Application.get_env(:wanderer_notifier, :corporation_exclude_list)
-
-      Application.put_env(:wanderer_notifier, :corporation_exclude_list, [
-        98_000_001,
-        98_000_002,
-        98_000_003
-      ])
+      with_corporation_exclude_list([98_000_001, 98_000_002, 98_000_003])
 
       assert Config.corporation_exclusion_enabled?()
-
-      # Restore original config
-      if original do
-        Application.put_env(:wanderer_notifier, :corporation_exclude_list, original)
-      else
-        Application.delete_env(:wanderer_notifier, :corporation_exclude_list)
-      end
     end
   end
 end
