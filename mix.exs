e27be09a6199f8ab05ca491905b9ecd9d57911b8
guarzo <guarzo.eve@gmail.com>
2026-02-05 09:46:41 -0700

    fix: standardize vars (#106)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WandererNotifier.MixProject do
   def project do
     [
       app: :wanderer_notifier,
-      version: "5.1.4",
+      version: "5.1.5",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
